c4bdcd50c5756b34f6531cd6100832e818501161
adomaitisc <caadomaitis@gmail.com>
2023-05-03 17:03:32 -0300

    Initial Commit

---
+++ SimpleList/SimpleListApp.swift
@@ -0,0 +1,17 @@
+//
+//  SimpleListApp.swift
+//  SimpleList
+//
+//  Created by Cauã Adomaitis on 5/3/23.
+//
+
+import SwiftUI
+
+@main
+struct SimpleListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
